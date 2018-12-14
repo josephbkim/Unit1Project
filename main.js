7fa0dbd6a01e032f772ec889ee4251ff818bda8e
joe kim <j80n89@gmail.com>
2018-12-14 11:25:03 -0500

    function 1 not working

---
+++ main.js
@@ -0,0 +1,7 @@
+$('body').css('background', 'purple');
+
+$('#playerButton').on('click', function (event) {
+    event.preventDefault;
+    let pNameInput = $('#playerInput').val;
+    $('#scoreName').html($('#playerInput').val);
+})
